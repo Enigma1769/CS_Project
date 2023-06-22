e3de8a1918dc52c48b28ca698fea1346dd45e4eb
Enigma1769 <enigma1769@proton.me>
2023-06-23 00:22:40 +0530

    minor fixes

--- 2/fibonacci.py
+++ 2/fibonacci.py
@@ -2,7 +2,7 @@ first=0
 second=1
 temp=0
 
-x=int(input("How many iterations do you want (>2)"))
+x=int(input("How many iterations do you want (>2):-"))
 print(first,second,end=" ")
 for i in range (2,x):
     temp = first+second
