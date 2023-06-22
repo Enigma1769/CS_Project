6d6342d5b90ca9b851a02a6752761dcce3935a39
Enigma1769 <enigma1769@proton.me>
2023-06-22 20:33:03 +0530

    remove country with w or h

--- 11/w_or_h.py
+++ 11/w_or_h.py
@@ -2,7 +2,7 @@ def count_wh():
     count = 0
 
     with open('Country.txt', 'r') as file:
-        data= file.readline()
+        data= file.readlines()
         for line in data:
             if line[0].lower() in ['w','h']:
                 count+=1
